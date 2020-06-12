59d17bca98205557fa4633fd5f60ef8d31a9786e
System User <dmueller@test3.cm.gemini>
2020-06-12 18:12:22 -0400

    rm things not necessary for data and embedding creation

--- evaluateWikilinksNED.sh
+++
@@ -1,2 +0,0 @@
-cd src
-python Experiment.py $1 $2 $3 $4 experiments/WikilinksNED/experiment.conf
--- idk.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='david', password='password',
-                                      host='127.0.0.1',
-                                                                    database='enwiki')
-cnx.close()
